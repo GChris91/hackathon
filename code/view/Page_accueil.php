ef0f9ff896b22fe445e9de0579e1ddc8587b3ccb
Anthony Laude <laudeanthony98@gmail.com>
2017-11-26 06:02:11 +1100

    yrd

--- code/view/Page_accueil.php
+++ code/view/Page_accueil.php
@@ -66,7 +66,7 @@
 
 <?php ob_start(); ?>
     <script>
-      // Note: This example requires that you consent to location sharing when 9999
+      // Note: This example requires that you consent to location sharing when 9999999
       // prompted by your browser. If you see the error "The Geolocation service
       // failed.", it means you probably did not give permission for the browser to
       // locate you.
