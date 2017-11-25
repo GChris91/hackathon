30aeaa68c6d871cc3e9f23423243007ac4906ea5
Anthony Laude <laudeanthony98@gmail.com>
2017-11-26 00:14:08 +1100

    Retour détails

--- code/view/Page_detail_feu.php
+++ code/view/Page_detail_feu.php
@@ -1,6 +1,6 @@
 <?php ob_start(); ?>
 	<div class="details">
-		<h1>Risque d'incendie</h1>
+		<h1><a href="/Hackathon/code"><i class="fa fa-chevron-circle-left" aria-hidden="true"></i></a> Risque d'incendie</h1>
 		<section class="rgba-green">
 			<h3 class="green">Vert</h3>
 			<span>
